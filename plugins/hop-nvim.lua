f6ed66e9d35ca02492c9ff82c0727d0012fa50b2
hsulab <ahcigar@foxmail.com>
2024-04-04 23:26:24 -0400

    update hop config

--- plugins/hop-nvim.lua
+++ plugins/hop-nvim.lua
@@ -4,5 +4,10 @@ return {
     event = "BufEnter", -- NOTE: It seems to not load without this.
     version = "*",
     opts = {},
+    config = function()
+      require("hop").setup({})
+      vim.api.nvim_set_keymap("n", "W", "<cmd>HopWord<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "E", "<cmd>HopLine<cr>", { silent = true })
+    end,
   }
 }
